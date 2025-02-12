23a4a4bf5d9fb6d93996cdcbdec0ec28c5274d50
Danylo Tymoshenko <dtymoshenko@griddynamics.com>
2025-02-12 12:51:03 +0100

    clean main

--- src/main/java/util/CheckResponseIsValid.java
+++
@@ -1,8 +0,0 @@
-package util;
-import io.restassured.response.Response;
-import org.testng.Assert;
-public class CheckResponseIsValid {
-    public static void checkResponseCode(Response response, int expectedStatusCode) {
-        Assert.assertEquals(response.getStatusCode(), expectedStatusCode);
-    }
-}
--- src/main/java/util/PrepareActualResponse.java
+++
@@ -1,9 +0,0 @@
-package util;
-
-import io.restassured.response.Response;
-
-public class PrepareActualResponse {
-    public static String prepareResponseAsString(Response response) {
-        return response.asString();
-    }
-}
