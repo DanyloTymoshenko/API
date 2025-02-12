1bbe4bca8cfa2aa077d5fbe5bdfe10d9619ffea7
Danylo Tymoshenko <dtymoshenko@griddynamics.com>
2025-02-12 12:58:59 +0100

    clean feature

--- src/main/java/util/CheckActualVsExpectedResponse.java
+++
@@ -1,9 +0,0 @@
-package util;
-
-import org.assertj.core.api.Assertions;
-import io.restassured.response.Response;
-public class CheckActualVsExpectedResponse {
-    public static void compareActualAndExpected(String actualResponse, String expectedResponse) {
-        Assertions.assertThat(actualResponse).isEqualTo(expectedResponse);
-    }
-}
